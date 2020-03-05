c09034ec96cc1232378c4fec165c1a55fb812ad4
Mieke Laporte <mieke.laporte@student.ucll.be>
2020-03-05 09:41:33 +0100

    changed name of servlet

--- src/ui/controller/Occurrences.java
+++ src/ui/controller/Occurrences.java
@@ -7,8 +7,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet("/Occurences")
-public class Servlet extends HttpServlet {
+@WebServlet("/Occurrences")
+public class Occurrences extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
     }
